88b6098eec198f3c6c54e114e818ea5e5b04dd27
Trishammk <154252848+Trishammk@users.noreply.github.com>
2024-01-05 09:28:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
-    const playerDiv = document.getElementById('player');
-    const fileInput = document.getElementById('fileInput');
-    const urlInput = document.getElementById('urlInput');
+const playerDiv = document.getElementById('player');
+const fileInput = document.getElementById('fileInput');
+const urlInput = document.getElementById('urlInput');
 
-    function playfilelink() {
-      const file = fileInput.files[0];
-      if (file) {
-        const objectUrl = URL.createObjectURL(file);
-        playerDiv.innerHTML = '<video controls width="400" height="300" src="${objectUrl}">/video>';
-          
-      }
+function playfilelink() {
+  const file = fileInput.files[0];
+  if (file) {
+    const objectUrl = URL.createObjectURL(file);
+    playerDiv.innerHTML = `<video controls width="400" height="300" src="${objectUrl}"></video>`;
+  }
+}
+
+function nowurl() {
+  const url = urlInput.value;
+  if (url) {
+    const videoId = extractVideoId(url);
+    if (videoId) {
+      const objectUrl = `https://www.youtube.com/embed/${videoId}`;
+      playerDiv.innerHTML = `<iframe width="400" height="300" src="${objectUrl}" frameborder="0" allowfullscreen></iframe>`;
+    } else {
+      console.error('Invalid YouTube URL');
     }
+  }
+}
 
-    function nowurl() {
-      const url = urlInput.value;
-      if (url) {
-        const videoId = extractVideoId(url);
-        if (videoId) {
-            playerDiv.innerHTML = `<video controls width="400" height="300" src="${objectUrl}"></video>`;
-
-          playerDiv.innerHTML = `<iframe width="400" height="300" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>`;
-
-         
-        } else {
-          console.error('Invalid YouTube URL');
-        }
-      }
-    }
-
-    function extractVideoId(url) {
-      const match = url.match(/[?&]v=([^?&]+)/);
-      return match && match[1];
-    }
+function extractVideoId(url) {
+  const match = url.match(/[?&]v=([^?&]+)/);
+  return match && match[1];
+}
